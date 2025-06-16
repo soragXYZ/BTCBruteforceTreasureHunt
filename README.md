3ebc71dd9829e386b87cfc72914835bbffc06158
soragXYZ <sorag.eth@protonmail.com>
2025-06-16 15:44:36 +0200

    add mnemonic verification

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-test
+Trying to bruteforce the mnemonic from this treasure hunt: https://bitcoin.fr/chasse-au-tresor/
+
+Usefull links:
+* https://iancoleman.io/bip39/#english
+* https://medium.com/@johncantrell97/how-i-checked-over-1-trillion-mnemonics-in-30-hours-to-win-a-bitcoin-635fe051a752
+* https://learnmeabitcoin.com/technical/keys/hd-wallets/mnemonic-seed/
--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"crypto/sha256"
 	"fmt"
 	"log"
+	"math/big"
 	"os"
 )
 
@@ -12,10 +14,10 @@ const (
 	frenchWordsFile  = "wordlist-fr.txt"
 )
 
-// Return the words as a map
-func fromFileToHash(filename string) map[string]int {
+// Load the 2048 words into memory, return them as a map
+func fromFileToHash(filename string) map[string]uint {
 
-	wordMap := make(map[string]int)
+	wordMap := make(map[string]uint)
 
 	// Open file
 	file, err := os.Open(filename)
@@ -27,7 +29,7 @@ func fromFileToHash(filename string) map[string]int {
 	// Create a new scanner to read the file line by line
 	scanner := bufio.NewScanner(file)
 
-	wordIndex := 1
+	wordIndex := uint(0)
 
 	// Loop through the file and read each line
 	for scanner.Scan() {
@@ -43,13 +45,93 @@ func fromFileToHash(filename string) map[string]int {
 
 	return wordMap
 }
+
+// For a given mnemonic of 24 words, return the number (bigInt) associated to it
+// which represents entropy + checksum
+func getEntropyAndChecksum(wordList map[string]uint, list []string) *big.Int {
+
+	number := big.NewInt(0)
+
+	for index, word := range list {
+
+		// Loop through each word and shift bits accordingly ->
+		// For example with 24 words:
+		// word pos 24 harbor (in binary: 01101001000): add value 01101001000
+		// word pos 23 excuse (in binary: 01001111000): add value 10011110000 00000000000
+		// word pos 22 harbor (in binary: 01101001000): add value 01101001000 00000000000 00000000000 etc...
+
+		// We are bigEndian -> most significant units are put first, so first word weights more than the 24th
+
+		number.Add(number, big.NewInt(0).Lsh(big.NewInt(int64(wordList[word])), uint(len(list)-1-index)*11))
+
+	}
+
+	// fmt.Printf("Entropy and checksum number is: %x\n", number)
+
+	return number
+}
+
+// Calculate if for a given entropy + checksum, the checksum is equal to the one included in the word
+func isSeedValid(entropyAndChecksum *big.Int, wordToVerify uint) bool {
+
+	// For 24 words, we have 256 % 32 = 8 bits of checksum
+	// So we shift by 8 bits to keep the entropy only and remove the checksum
+	// Then we calculate the sha256 of this entropy
+	hash := sha256.Sum256(entropyAndChecksum.Rsh(entropyAndChecksum, 8).Bytes())
+
+	// fmt.Printf("Calculated hash: %x\n", hash)
+	// fmt.Printf("Calculated checksum:        %011b\n", hash[0])
+	// fmt.Printf("Word to verify:             %011b\n", wordToVerify)
+	// fmt.Printf("checksum in word to verify: %011b\n", wordToVerify&255)
+
+	// Then we compare the checksum we calculated which is the first 8 bits of the hash (hash[0] here)
+	// with the checksum included in the last word
+	// We only keep the last 8 bit of the last word (2^8 = 255), corresponding to the checksum
+	return uint(hash[0]) == wordToVerify&255
+
+}
+
 func main() {
 
 	enMap := fromFileToHash(englishWordsFile)
-	frMap := fromFileToHash(frenchWordsFile)
+	// frMap := fromFileToHash(frenchWordsFile)
 
-	fmt.Println(enMap)
-	fmt.Println(frMap)
+	verifierWords := []string{
+		"alien",
+		"detect",
+		"flip",
+		"gas",
+		"organ",
+		"pleasant",
+		"staff",
+		"trigger",
+		"stumble", // for test, correct
+		"stumble", // for test, correct
+	}
 
-	// fmt.Printf("Left shift by 2 bits: %08b (%d)\n", 1<<3, 1<<3)
+	preSeed := []string{"spawn", "mass", "begin", "twist", "sausage", "other", "race", "amateur", "nasty", "follow", "rookie", "leader", "kiwi", "normal", "rifle", "square", "soap", "fault", "pet", "involve", "evolve", "call", "canyon"}
+
+	// We know that every mnemonic made from the first 23 words and a verifier word are valid
+	// So we loop until we found 23 words which are compatible with the given verifier word list
+
+	for _, word := range verifierWords {
+
+		mnemonic := append(preSeed, word)
+
+		// fmt.Println("Trying mnemonic: ", mnemonic)
+
+		entropyAndChecksum := getEntropyAndChecksum(enMap, mnemonic)
+
+		if isSeedValid(entropyAndChecksum, enMap[word]) {
+			fmt.Println("Mnemonic found ! ", mnemonic)
+			return
+
+		} else {
+			// 1 verifier word invalid means that one of the 23 words used is invalid
+			// Process to the next iteration
+			continue
+		}
+
+	}
+
 }
